2576c2555aff67a89f6218bcc792d20272bd3898
Lazer <me@liza.io>
2017-01-28 14:36:40 +0100

    Exercise 3-2, graph A

---
+++ Learning-TensorFlow/Example3-2/graph-a.py
@@ -0,0 +1,23 @@
+# Example instructing reader to create graph based on provided visual representation.
+# Coincidentally also learned how to start TensorBoard in this example 
+# to test the graph visualization (making sure it matches the instruction).
+
+import tensorflow as tf  
+
+a = tf.constant(5,name="a")
+b = tf.constant(10,name="b")
+
+c = tf.mul(a,b,name="c") 
+d = tf.add(a,b,name="d")
+
+e = tf.sub(c,d,name="e")
+f = tf.add(d,c,name="f")
+
+g = tf.truediv(e,f,name="g")
+
+with tf.Session() as sess:
+    result = sess.run(g)
+    summary_writer = tf.summary.FileWriter("/tmp/tensorflow", sess.graph)
+
+print("Result = {}".format(result))
+
